7467d5afb8d0a6e8b097e02a29c3c94611289bde
notitle12 <notitle1215@gmail.com>
2023-08-08 17:50:10 +0900

    회원가입 및 로그인, 관리자 권한 설정, 스프링시큐리티 회원가입 및 로그인 프론트 작성

--- src/main/java/com/rooi/rooi/controller/UserController.java
+++ src/main/java/com/rooi/rooi/controller/UserController.java
@@ -1,4 +1,54 @@
 package com.rooi.rooi.controller;
 
+import com.rooi.rooi.dto.LoginRequestDto;
+import com.rooi.rooi.dto.SignupRequestDto;
+import com.rooi.rooi.service.UserService;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/api")
 public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/user/login-page")
+    public String loginPage() {
+        return "login";
+    }
+
+    @GetMapping("/user/signup")
+    public String signupPage() {
+        return "signup";
+    }
+
+    @PostMapping("/user/signup")
+    public String signup(@ModelAttribute SignupRequestDto requestDto) { //Dto -> 로 생성자 생성
+
+        userService.signup(requestDto);
+
+        return "redirect:/api/user/login-page";
+
+    }
+//    필터에서 처리
+//    @PostMapping("/user/login")
+//    public String login(LoginRequestDto requestDto, HttpServletResponse res) {
+//        try {
+//            userService.login(requestDto, res);
+//        } catch (Exception e) {
+//            return "redirect:/api/user/login-page?error";
+//        }
+//
+//        return "redirect:/";
+//
+//    }
 }
