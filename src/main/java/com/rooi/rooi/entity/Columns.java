5b2887368df9b2bdd454c720df705907352b8c2b
sehun <ban6276@gmail.com>
2023-08-08 18:56:21 +0900

    컬럼 기능 1차

--- src/main/java/com/rooi/rooi/entity/Columns.java
+++ src/main/java/com/rooi/rooi/entity/Columns.java
@@ -1,4 +1,27 @@
 package com.rooi.rooi.entity;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@Entity
+@NoArgsConstructor
+@Table(name="Columns")
 public class Columns {
+
+    @Id
+    @GeneratedValue
+    @Column(name ="columns")
+    private Long id;
+
+    private String title;
+
+    public Columns(Board board, String title) {
+//        this.board = board;
+        this.title = title;
+    }
+
 }
--- src/main/java/com/rooi/rooi/security/UserDetailsImpl.java
+++ src/main/java/com/rooi/rooi/security/UserDetailsImpl.java
@@ -1,4 +1,65 @@
 package com.rooi.rooi.security;
 
-public class UserDetailsImpl {
-}
+import com.rooi.rooi.entity.User;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+
+public class UserDetailsImpl implements UserDetails {
+
+ private final User user;
+
+    public UserDetailsImpl(User user) {
+        this.user = user;
+    }
+
+    public User getUser(){
+     return user;}
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getUsername();
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+
+
+        return null;
+    }
+
+
+    //계정이 만료되지 않았는지 여부 반환
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+    //계정이 잠겨있지 않은지 여부 반환
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+    //사용자의 자격 증명이 만료되지 않았는지 여부를 반환
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+    //사용자가 활성화 되었는지 여부를 반환
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+ }
+
+
+
+
+
+
