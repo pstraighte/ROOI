2c66783df7d44e30d51f8a15c3318dd8f19e5c93
hakjunjoo <hakjun0918@nate.com>
2023-08-09 20:42:34 +0900

    작업자 기능 추가

---
+++ src/main/java/com/rooi/rooi/dto/WorkerRequestDto.java
@@ -0,0 +1,8 @@
+package com.rooi.rooi.dto;
+
+import lombok.Getter;
+
+@Getter
+public class WorkerRequestDto {
+    private String worker;
+}
---
+++ src/main/java/com/rooi/rooi/entity/Worker.java
@@ -0,0 +1,27 @@
+package com.rooi.rooi.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class Worker {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "card_id")
+    private Card card;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    public Worker(Card card, User newWorker) {
+        this.card = card;
+        this.user = newWorker;
+    }
+}
