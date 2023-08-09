da6bc503220575b144eb1f0b13c24677a9e54a45
pstraighte <pstraighte@gmail.com>
2023-08-09 23:48:45 +0900

    ✨카드 마감일 기능 재추가(머지때 실수로 날린듯,,)

--- src/main/java/com/rooi/rooi/dto/CardRequestDto.java
+++ src/main/java/com/rooi/rooi/dto/CardRequestDto.java
@@ -2,8 +2,11 @@ package com.rooi.rooi.dto;
 
 import lombok.Getter;
 
+import java.time.LocalDate;
+
 @Getter
 public class CardRequestDto {
     private String title;
     private String description;
+    private LocalDate deadLine;
 }
--- src/main/java/com/rooi/rooi/dto/CardResponseDto.java
+++ src/main/java/com/rooi/rooi/dto/CardResponseDto.java
@@ -3,16 +3,20 @@ package com.rooi.rooi.dto;
 import com.rooi.rooi.entity.Card;
 import lombok.Getter;
 
+import java.time.LocalDate;
+
 @Getter
 public class CardResponseDto {
     private String title;
     private String description;
     private Long columnId;
+    private LocalDate deadLine;
 
 
     public CardResponseDto(Card card) {
         this.title = card.getTitle();
         this.description = card.getDescription();
         this.columnId = card.getColumns().getId();
+        this.deadLine = card.getDeadLine();
     }
 }
