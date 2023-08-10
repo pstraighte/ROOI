59dd925c650da5224f265a5efd83432503406f6b
notitle12 <notitle1215@gmail.com>
2023-08-10 20:18:26 +0900

    프론트 멸망 직전

--- src/main/java/com/rooi/rooi/controller/HomeController.java
+++ src/main/java/com/rooi/rooi/controller/HomeController.java
@@ -1,10 +1,13 @@
 package com.rooi.rooi.controller;
 
+import com.rooi.rooi.dto.ColumnsResponseDto;
 import com.rooi.rooi.security.UserDetailsImpl;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Controller
 public class HomeController {
@@ -14,4 +17,5 @@ public class HomeController {
         model.addAttribute("username", userDetails.getUsername());
         return "index";
     }
+
 }
